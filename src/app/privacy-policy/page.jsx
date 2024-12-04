d652ac52e2868c1e26686f85ef1532096d97c688
Amol Kalel <kalelamol07@gmail.com>
2024-12-04 20:07:40 +0530

    changes by ak on front

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -1,21 +1,17 @@
-"use client"
-
-
+"use client";
 
 import Cursor from "@/components/cursor/Cursor";
 import Footer from "@/components/layouts/footer/footer";
 import Navbar from "@/components/layouts/navbar/navbar";
 import Privacy from "@/components/policies/privacy/Privacy";
 
-
-
 const Page = () => {
   return (
     <div>
-        <Cursor/>
-    <Navbar/> 
-    <Privacy/>
-    <Footer/>
+      <Cursor />
+      <Navbar />
+      <Privacy />
+      <Footer />
     </div>
   );
 };
