ab24727ee3872741e3b8501244eeb96f34044d15
Amol Kalel <kalelamol07@gmail.com>
2024-12-04 20:10:26 +0530

    changes by ak on front

--- src/app/coordinator/page.jsx
+++ src/app/coordinator/page.jsx
@@ -1,21 +1,17 @@
-"use client"
-
+"use client";
 
 import Form from "@/components/coordinator/Form";
 import Cursor from "@/components/cursor/Cursor";
 import Footer from "@/components/layouts/footer/footer";
 import Navbar from "@/components/layouts/navbar/navbar";
 
-
-
 const Page = () => {
   return (
     <div>
-      <Cursor/>
-    <Navbar/> 
-
-   <Form/>
-    <Footer/>
+      <Cursor />
+      <Navbar />
+      <Form />
+      <Footer />
     </div>
   );
 };
--- src/app/verifycertificate/page.jsx
+++ src/app/verifycertificate/page.jsx
@@ -1,19 +1,18 @@
-
-import Cursor from '@/components/cursor/Cursor'
-import Footer from '@/components/layouts/footer/footer'
-import Navbar from '@/components/layouts/navbar/navbar'
-import VerifyCertificate from '@/components/verify/verifycertificate'
-import React from 'react'
+import Cursor from "@/components/cursor/Cursor";
+import Footer from "@/components/layouts/footer/footer";
+import Navbar from "@/components/layouts/navbar/navbar";
+import VerifyCertificate from "@/components/verify/verifycertificate";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='w-screen h-screen overflow-x-hidden'>
-        <Cursor/>
-       <Navbar/>
-       <VerifyCertificate/>
-       <Footer/>
+    <div className="w-screen h-screen overflow-x-hidden">
+      <Cursor />
+      <Navbar />
+      <VerifyCertificate />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
